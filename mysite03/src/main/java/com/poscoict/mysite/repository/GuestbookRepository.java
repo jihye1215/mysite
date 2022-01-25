4f3c1d552d93ac5bf94e9b9f52934e8331a99805
wlgp1 <wlgp1@DESKTOP-5T5NIVG>
2022-01-25 19:35:43 +0900

    mysite03 방명록 finish

--- mysite03/src/main/java/com/poscoict/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/poscoict/mysite/repository/GuestbookRepository.java
@@ -6,7 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,103 +30,14 @@ public class GuestbookRepository {
    }
       
    public boolean delete(Long no, String password) {
-	      
-	      boolean result = false;
-	      Connection conn = null;
-	      PreparedStatement pstmt = null;
-	      ResultSet rs = null;
-	      
-	      try {
-	         conn = getConnection();
-	         
-	         //3. SQL 준비
-	         String sql = "delete from guestbook where no = ? and password = ?";
-	         pstmt = conn.prepareStatement(sql);
-
-	         //4. 바인딩(binding)   
-	         pstmt.setLong(1, no);
-	         pstmt.setString(2, password);
-	         
-	         //5. SQL 실행 , executeQuery는 rs, executeUpdate는 int로 반환한다. 
-	         result = (pstmt.executeUpdate() == 1);
-  
-	      } catch (SQLException e) {
-	         System.out.print("error : " + e); 
-	      }
-	      
-	      finally {
-	         try {
-	            if(rs != null) {
-	               rs.close();
-	            }
-	            if(pstmt != null) {
-	               pstmt.close();
-	            }
-	            if(conn != null) {
-	               conn.close();
-	            }
-	         } catch(SQLException e) {
-	            e.printStackTrace();
-	         }
-	      }
-	      
-	      return result;
-	   }
-	   
+	   Map<String, Object> map = new HashMap<>();
+	   map.put("no", no);
+	   map.put("password", password);
+	   return sqlSession.delete("guestbook.delete", map) == 1;
+   }
+	    
    public int selectCnt(){
-	      
-	      Connection conn = null;
-	      PreparedStatement pstmt = null;
-	      ResultSet rs = null;
-	      int result = 0;
-	      
-	      try {
-	         conn = getConnection();
-
-	         String sql = "select count(*) from guestbook";
-	         pstmt = conn.prepareStatement(sql);
-
-	         rs = pstmt.executeQuery();
-
-	         if(rs.next()) {
-	            result = rs.getInt(1);
-	         }
-	         
-	      } catch (SQLException e) {
-	         System.out.print("error : " + e); 
-	      }
-	      
-	      finally {
-	         try {
-	            if(rs != null) {
-	               rs.close();
-	            }
-	            if(pstmt != null) {
-	               pstmt.close();
-	            }
-	            if(conn != null) {
-	               conn.close();
-	            }
-	         } catch(SQLException e) {
-	            e.printStackTrace();
-	         }
-	      }
-	      return result;
-	   }
-
-   
-   private Connection getConnection() throws SQLException {
-      Connection conn = null;
-      try {
-         // 1. JDBC 드라이버 로딩
-         Class.forName("com.mysql.cj.jdbc.Driver");
-         // 2. 연결하기
-         String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
-         conn = DriverManager.getConnection(url, "webdb", "webdb");
-      } catch(ClassNotFoundException e) {
-         System.out.print("드라이버 로딩 실패 : "+e);
-      }
-      return conn;
+	   return sqlSession.selectOne("guestbook.selectCnt");
    }
 
 }
