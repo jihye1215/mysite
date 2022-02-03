a896ca575c32b70caff54af33737bee656af5d36
wlgp1 <wlgp1@DESKTOP-5T5NIVG>
2022-02-03 10:45:38 +0900

    mysite03 validation #1 add

--- mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.poscoict.mysite.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,9 +31,18 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
-	public String join(UserVo userVo) {
-		userService.join(userVo);
-		System.out.println(userVo);
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError error : list) {
+//				System.out.println(error);
+//			}
+//			model.addAttribute("userVo", userVo);
+			// map으로 들어간다.
+			model.addAllAttributes(result.getModel());
+			return "user/join";
+		}
+		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 	
