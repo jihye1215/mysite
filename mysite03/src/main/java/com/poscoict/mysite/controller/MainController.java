51a49ecdfff8f2fd02c857fad0f3439142c50165
jihye1215 <wlgp1494@naver.com>
2022-02-28 15:50:10 +0900

    ajax 적용 #1

--- mysite03/src/main/java/com/poscoict/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.poscoict.mysite.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -10,5 +14,20 @@ public class MainController {
 	public String index() {
 		// return "/WEB-INF/views/main/index.jsp";
 		return "main/index";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg01")
+	public String message01() {
+		return "안녕";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg03")
+	public Object message03() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("messae", "Hello World");
+		
+		return map;
 	}
 }
